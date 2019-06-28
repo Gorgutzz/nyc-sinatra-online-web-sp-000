2468c79ba6605475716a4bbf3b1d5e74958493d1
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-06-28 00:59:35 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,48 +1,51 @@
 class FiguresController < ApplicationController
+  # add controller methods
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
 
- get '/figures/new' do
-   @titles = Title.all
-   @landmarks = Landmark.all
-   erb :'figures/new'
- end
+  post '/figures' do
+   @figure=Figure.create(params[:figure])
 
- post '/figures' do
-   f = Figure.create(params[:figure])
    if !params[:title][:name].empty?
-     f.titles << Title.create(params[:title])
+     @figure.titles << Title.create(params[:title])
    end
+
    if !params[:landmark][:name].empty?
-     f.landmarks << Landmark.create(params[:landmark])
+     @figure.landmarks << Landmark.create(params[:landmark])
    end
- end
+  end
 
- get '/figures' do
-   @figures = Figure.all
-   erb :'/figures/index'
- end
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
 
- get '/figures/:id' do
-   @figure = Figure.find(params[:id])
-   erb :'/figures/show'
- end
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
 
- get '/figures/:id/edit' do
-   @figure = Figure.find(params[:id])
-   @titles = Title.all
-   @landmarks = Landmark.all
-   erb :'/figures/edit'
- end
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/edit'
+  end
 
- patch '/figures/:id' do
-   @figure = Figure.find(params[:id])
-   @figure.update(params[:figure])
-   if !params[:title][:name].empty?
-     f.titles << Title.create(params[:title])
-   end
-   if !params[:landmark][:name].empty?
-     f.landmarks << Landmark.create(params[:landmark])
-   end
-   redirect "/figures/#{@figure.id}"
- end
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
 
-end
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    redirect "/figures/#{@figure.id}"
+  end
